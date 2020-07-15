b332e4e36d4f762cbc485a5a06b902c0293f8ca8
JRIGS <raczkowskijonathan@gmail.com>
2020-07-14 20:04:55 -0400

    Finished all but 2 functions for now

--- index.js
+++ index.js
@@ -4,38 +4,38 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-var votingAge = 18;
+// var votingAge = 18;
 
-if (votingAge >= 18) {
-    console.log("You are old enough to vote!");
-}
+// if (votingAge >= 18) {
+//     console.log("You are old enough to vote!");
+// }
 
-else {
-    console.log("You are not old enough to vote!");
-  }
+// else {
+//     console.log("You are not old enough to vote!");
+//   }
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
-    var month = "May";
+// var month = "May";
 
-    if(month === "May"){
-        var month = "Summer time"
-    }
-    else{
-        var month = "Off season"
-    }
+// if(month === "May"){
+//     var month = "Summer time"
+// }
+// else{
+//     var month = "Off season"
+// }
 
-    console.log(month);
+// console.log(month);
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-var x = "1999"
+// var x = "1999"
 
-Number(x);
+// Number(x);
 
-console.log(x);
+// console.log(x);
 
 // OR
 
@@ -49,24 +49,27 @@ console.log(x);
 
 //Task d: Write a function to multiply a*b 
 
-function multiply(num1, num2){
+// function multiply(num1, num2){
 
-return num1 * num2;
+// return num1 * num2;
 
-}
+// }
 
-let num1 = '2'
-let num2 = '5'
+// let num1 = '2'
+// let num2 = '5'
 
-console.log(multiply(num1, num2));
+// console.log(multiply(num1, num2));
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
+// function getDogYears(humanAge) {
+//         return humanAge * 7;
+//     }
 
-
-
+//     console.log("You are " + getDogYears(7) + " years old in dog years");
+    
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
@@ -85,7 +88,10 @@ console.log(multiply(num1, num2));
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+
+// const dogFeeder = (weight, age) => {
+
+// }
 
 
 
@@ -97,19 +103,40 @@ console.log(multiply(num1, num2));
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+// Miles to Kilo
+// function getKilometers(i) {
+//      return i*1.60935;
+// }
+// // Kilo to Miles
+// function getMiles(i) {
+//      return i*0.6213688756;
+// }
 
+// console.log(getKilometers(3) + " Miles");
 
+// console.log(getMiles(3) + " Kilometers");
 
+// https://www.calculator.net/conversion-calculator.html
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+
+// function getFeet(i) {
+//     return i*30.48;
+// }
+// function getCentimeters(i) {
+//     return i*0.032808399;
+// }
+
+// console.log(getFeet(5) + " centimeters");
+
+// console.log(getCentimeters(5) + " feet");
 
 
 
@@ -118,9 +145,40 @@ console.log(multiply(num1, num2));
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
 
+// var bottles = 99;
 
+// function annoyingSong(num){
+
+//     if(num <= 99 && num >= 0){
+//     num--;
+
+//     return num;
+//     }
+// }
+
+// console.log(annoyingSong(bottles));
+
+// function annoyingSong() {
+//     var bottles;
+//     var bottlesLeft;
+//     for (i = 99; i >= 1; i--) {
+
+//         if (i == 1) {
+//             bottles = "bottle";
+//             bottlesLeft = "No bottles of beer on the wall!";
+//         } else {
+//             bottles = "bottles";
+//             bottlesLeft = i - 1 + " bottles of beer on the wall!";
+//         }
+//         console.log(i + " " + bottles + " of beer on the wall,");
+//         console.log(i + " " + bottles + " of beer,");
+//         console.log("Take one down, pass it around,");
+//         console.log(bottlesLeft);
+//     }
+
+// }
+// console.log(annoyingSong());
 
 
 /************************************************************** Task 7 **************************************************************/
@@ -132,34 +190,34 @@ console.log(multiply(num1, num2));
 //60s should be D 
 //and anything below 60 should be F
 
-function finalGrade(total){
-    
-    if(total >= 90 && total <= 100){
-        console.log("A");
-    }
+// function finalGrade(total){
 
-    if(total >= 80 && total <= 89){
-        console.log("B");
-    }
-    
-    if(total >= 70 && total <= 79){
-        console.log("C");
-    }
+//     if(total >= 90 && total <= 100){
+//         console.log("A");
+//     }
 
-    if(total >= 60 && total <= 69){
-        console.log("D");
-    }
+//     if(total >= 80 && total <= 89){
+//         console.log("B");
+//     }
 
-    if(total <= 59){
-        console.log("F");
-    }
+//     if(total >= 70 && total <= 79){
+//         console.log("C");
+//     }
 
-}
-console.log(finalGrade(62));
-  
+//     if(total >= 60 && total <= 69){
+//         console.log("D");
+//     }
 
-  
-  
+//     if(total <= 59){
+//         console.log("F");
+//     }
+
+// }
+// console.log(finalGrade(62));
+
+
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
@@ -173,8 +231,3 @@ console.log(finalGrade(62));
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
 //update your rock papers sissors code below to take a prompt from a user using the window object
-
-
-
-
-
